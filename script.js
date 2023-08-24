41c7e42d7dd439242cdc6207afd6dc0fb3ca27fb
Nishant Rai <rnishant9970@gmail.com>
2023-08-24 13:24:54 +0530

    All the functionalities are added

--- script.js
+++ script.js
@@ -26,12 +26,17 @@ function handleSlider(){
         inputSlider.value = passwordLength;
         lengthDisplay.innerText = passwordLength;
         //or kuch bhi karna chahiye
+         
+        const min = inputSlider.min;
+        const max = inputSlider.max;
+        inputSlider.style.backgroundSize = (passwordLength-min)*100/(max-min)+"% 100%";
 
 }
 
 function setIndicator(color){
        indicator.style.backgroundColor = color;
        //shadow
+       indicator.style.boxShadow = "0px 0px 12px 1px ${color}";
 
 }
 
